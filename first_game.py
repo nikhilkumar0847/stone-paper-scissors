40c6f142453ba181a2f487382d954439aa305bf4
srivastva06 <nikhil164234@gmail.com>
2026-08-17 20:04:27 +0530

    Code update

--- first_game.py
+++ first_game.py
@@ -1,4 +1,10 @@
 # Stone_paper_scissors game
 
 def game():
-    
+    game = [("stone", "paper","Scissor"),
+            ("stone", "scissor","paper"),
+            ("paper", "stone","scissor"),
+            ("paper", "scissor","stone"),
+            ("scissor","paper","stone"),
+            ("scissor","stone","paper"),
+            ]
